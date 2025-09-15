4fbd3beba8a859a01854573825766244b2a31061
rezamaulana13 <rezamaulanaply@gmail.com>
2025-09-15 15:58:56 +0700

    photo

--- database/migrations/2024_12_01_102529_create_reviews_table.php
+++ database/migrations/2024_12_01_102529_create_reviews_table.php
@@ -2,28 +2,27 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('client_id')->unsigned();
-            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('user_id');
             $table->text('comment')->nullable();
             $table->string('rating')->nullable();
-            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('media')->nullable(); // 📸 untuk foto/video review
             $table->string('status')->default(0);
             $table->timestamps();
+
+            // relasi
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('reviews');
